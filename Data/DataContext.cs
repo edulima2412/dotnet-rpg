25ae2f8e3e329b0b7491f2bc23debf1e8dac82ae
Eduardo Lima <edulima2412@gmail.com>
2021-03-08 13:52:07 -0300

    create migrations and class weapon

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -6,8 +6,8 @@ namespace dotnet_rpg.Data
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
-
         public DbSet<Character> Characters { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Weapon> Weapons { get; set; }
     }
 }
